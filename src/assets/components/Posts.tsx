a145fb2827b50aa2a6313fbe0a6ac38bcce729fd
DaneVN <Dane.vN23@gmail.com>
2024-11-21 20:18:32 +0200

    Fetch Logic + attempt at SinglePost rendering, Mini React error #31 in console

--- src/assets/components/Posts.tsx
+++ src/assets/components/Posts.tsx
@@ -1,28 +1,43 @@
-import React from "react";
 import { useState } from "react";
 import SinglePost from "./SinglePost";
 
 function Posts() {
   //logic for api call and data fetching
-  const [Error, setError] = useState(null);
-  let postArray: object[];
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((res) => res.json())
-    .then((data) => {
-      postArray = data;
-    })
-    .catch((err) => {
-      setError(err);
-    });
+  const [error, setError] = useState(null);
 
-  React.useEffect(() => {
-    console.log(postArray);
-  });
+  interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  }
+
+  async function createPosts() {
+    await fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Responce came back not ok");
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        return data.map((item: Post) => (
+          <SinglePost
+            title={`${item.id}. ${item.title}`}
+            body={item.body}
+          ></SinglePost>
+        ));
+      })
+      .catch((err) => {
+        setError(err);
+      });
+  }
 
   return (
     <>
-      <SinglePost title="" body=""></SinglePost>
-      {Error && (
+      {createPosts()}
+      {error && (
         <div api-error>
           <h2>Looks like something went wrong with the server.</h2>
           <h4> Please try again in a few minutes.</h4>
